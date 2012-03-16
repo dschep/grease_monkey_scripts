d6bad26328eb7042573a7d72134bebccd93a535b
Daniel Schep <dschep@gmail.com>
2012-03-16 18:15:52 -0400

    a reddit script that just marks things hidden as you scroll past them

---
+++ Reddit_Scroll-Spy-Hide.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name           Reddit Scroll-Spy-Hide
+// @namespace      net.dschep.reddit-scrollspyhide
+// @description    Hides posts as you scroll past them.
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
+// @include        http://www.reddit.com/*
+// ==/UserScript==
+
+$(document).ready(function () {
+    var things = $('.things');
+    $(window).scroll(function () {
+        var scrollTop= $('body').scrollTop();
+        $('.link').not('.hidden').filter(function () {
+            return ($(this).offset().top < scrollTop || // off screen
+                    // or at bottom of page
+                    scrollTop >= $('body').height() - $(window).height() - 10)
+        }).each(function () {
+            var $this = $(this);
+            $.post('/api/hide', {
+                executed: 'hidden',
+                id: $this.data('fullname'),
+                renderstyle: 'html',
+                uh: $('[name=uh]').val()
+            }, function () {
+                $this.addClass('hidden')
+                    .find('.hide-button a:contains(hide)')
+                    .attr('onclick', "change_state(this, 'unhide', hide_thing);")
+                    .text('unhide');
+            });
+        });
+    });
+});
